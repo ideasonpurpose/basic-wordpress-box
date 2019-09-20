e70f388e5c5eb45ababa40b311aaa2ae9bfa54f8
Joe Maller <joe@ideasonpurpose.com>
2019-09-20 18:05:56 -0400

    tweak build script to remove host traces

--- build.sh
+++ build.sh
@@ -8,8 +8,18 @@ set -e    # script should exit on error
 vagrant box update
 vagrant up --provision
 vagrant halt
+
+
+VBoxManage sharedfolder remove packer-base --name vagrant
+#VBoxManage modifyvm packer-base --uart1 off
+VBoxManage modifyvm packer-base --audio none
+VBoxManage modifyvm packer-base --uartmode1 disconnected
+
+
 mkdir -p box
 rm -f box/*.{ovf,vmdk}
+
 VBoxManage export packer-base --ovf20 -o box/basic-wp.ovf
+
 PACKER_LOG=1 packer build basic-wp.json
 vagrant box add --force basic-wp box/virtualbox-basic-wp.box
